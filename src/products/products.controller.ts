adc4785c460b8658835738ae694a64d476322497
Angel Javier Puc Yamá <angelpuc08@gmail.com>
2024-06-29 16:19:22 -0600

    feat: :sparkles: initial commit

---
+++ src/products/products.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, ParseIntPipe } from '@nestjs/common';
+import { ProductsService } from './products.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationDto } from '../common/dto';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+
+@Controller('products')
+export class ProductsController {
+  constructor(private readonly productsService: ProductsService) {}
+
+  @MessagePattern({ cmd: 'createProduct' })
+  create(@Payload() createProductDto: CreateProductDto) {
+    return this.productsService.create(createProductDto);
+  }
+
+  @MessagePattern({ cmd: 'getProducts' })
+  findAll(@Payload() paginationDto: PaginationDto) {
+    return this.productsService.findAll(paginationDto);
+  }
+
+  @MessagePattern({ cmd: 'getProduct' })
+  findOne(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.findOne(id);
+  }
+
+  @MessagePattern({ cmd: 'updateProduct' })
+  update(@Payload() updateProductDto: UpdateProductDto) {
+    return this.productsService.update(updateProductDto.id, updateProductDto);
+  }
+
+  @MessagePattern({ cmd: 'deleteProduct' })
+  remove(@Payload('id', ParseIntPipe) id: number) {
+    return this.productsService.remove(id);
+  }
+}
